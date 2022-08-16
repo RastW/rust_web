40005e0144e1b8e46edd334aa2047bc7ee0c8d70
RastW <rast.leonhart@qq.com>
2022-08-17 00:16:08 +0800

    HTTP request analysis

--- http/src/httprequest.rs
+++ http/src/httprequest.rs
@@ -1,5 +1,7 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, env::var};
 
+// Notes:
+// * Run &`Cargo test --bin d23` to check your program
 #[derive(Debug, PartialEq)]
 pub enum Method {
     Post,
@@ -54,25 +56,25 @@ impl From<String> for HttpRequest {
         let mut parsed_version = Version::V1_1;
         let mut parsed_resource = Resource::Path(String::from(""));
 
-        let mut headers = HashMap::new();
-        let mut parsed_msg_body = String::from("");
+        let mut parsed_headers = HashMap::new();
+        let mut parsed_msg_body = "";
 
         for line in req.lines() {
-            if line.contains("http") {
+            if line.contains("HTTP") {
                 // 请求行
-                let (method, resource, version) = process_req_line(req);
+                let (method, resource, version) = process_req_line(line);
                 parsed_method = method;
                 parsed_resource = resource;
                 parsed_version = version;
             } else if line.contains(":") {
                 // head
-                let (key, value) = process_req_header(req);
-                headers.insert(key, value);
+                let (key, value) = process_req_header(line);
+                parsed_headers.insert(key, value);
             } else if line.len() == 0 {
                 // 空行
-
             } else {
                 // 消息体
+                parsed_msg_body = line;
             }
         }
 
@@ -80,10 +82,37 @@ impl From<String> for HttpRequest {
             method: parsed_method,
             version: parsed_version,
             resource: parsed_resource,
-            headers: headers,
-            msg_body: parsed_msg_body,
-        };
+            headers: parsed_headers,
+            msg_body: parsed_msg_body.to_string(),
+        }
     }
+}
+
+fn process_req_line(line: &str) -> (Method, Resource, Version) {
+    let mut words = line.split_whitespace();
+    let method = words.next().unwrap();
+    let resource = words.next().unwrap();
+    let version = words.next().unwrap();
+
+    (
+        method.into(),
+        Resource::Path(resource.to_string()),
+        version.into()
+    )
+}
+
+fn process_req_header(line: &str) -> (String, String) {
+    let mut header_iters = line.split(":");
+    let mut key = String::from("");
+    let mut value = String::from("");
+    if let Some(k) = header_iters.next() {
+        key = k.to_string();
+    }
+
+    if let Some(v) = header_iters.next() {
+        value = v.to_string();
+    }
+    (key, value)
 }
 
 #[cfg(test)]
@@ -100,5 +129,21 @@ mod tests {
     fn test_version_into() {
         let m: Version = "HTTP/1.1".into();
         assert_eq!(m, Version::V1_1);
+    }
+
+    #[test]
+    fn test_read_http() {
+        let s = String::from("GET /greeting HTTP/1.1\r\nHost:localhost:8080");
+        let mut headers_expected = HashMap::new();
+        headers_expected.insert("Host".into(), "localhost".into());
+        headers_expected.insert("Accept".into(), "*/*".into());
+        headers_expected.insert("User-Agent".into(), "curl/7.71.1".into());
+
+        let req: HttpRequest = s.into();
+        println!("{:#?}", req);
+        assert_eq!(Method::Get, req.method);
+        assert_eq!(Version::V1_1, req.version);
+        assert_eq!(Resource::Path("/greeting".to_string()), req.resource);
+        assert_eq!(headers_expected, req.headers);
     }
 }
